50957975d88e2d8922189156d06845204ee1896a
hagemon <ooonefolder@gmail.com>
2023-10-12 23:39:03 +0800

    license; fix bug for ps1

--- bin/pvw.ps1
+++ bin/pvw.ps1
@@ -3,14 +3,14 @@ param(
     [string[]]$params
 )
 
-$PIPE_NAME = ".\_envs._cfg"
+$PIPE_NAME = "$HOME\.pvw\_envs._cfg"
 
 if ($params.Length -gt 0) {
     # & "python" "-m" "cProfile" "-s" "time" "main.py" $params # for evaluating execution time
     & pvw-py $params
     if (($params[0] -eq "activate") -and (Test-Path $PIPE_NAME)) {
         # activate parameter will create a _envs._cfg file as a pipe to make communication between parent/child process.
-        $ps1Path = Get-Content "_envs._cfg"
+        $ps1Path = Get-Content $PIPE_NAME
         & $ps1Path
         Remove-Item $PIPE_NAME
     }
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import sys
 
 if sys.platform.startswith("win"):
@@ -9,11 +9,23 @@ else:
 setup(
     name="pvw",
     version="0.0.1",
+    readme="README.md",
+    requires_python="3.6",
     packages=["pvw"],
+    author="hagemon",
+    author_email="ooonefolder@gmail.com",
+    keywords="venv",
+    description="a lightweight python venv wrapper to manage venvs",
+    license="MIT",
     package_dir={
         "pvw": "pvw"
     },
     install_requires=['importlib-metadata; python_version >= "3.6"', "termcolor"],
     entry_points={"console_scripts": ["pvw-py = pvw.main:main"]},
-    scripts=[f"bin/{shell_file}"]
+    scripts=[f"bin/{shell_file}"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    url="https://github.com/hagemon/pvw"
 )
